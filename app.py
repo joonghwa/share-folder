848c18f593abbf9fe9e7f1f4615bb37832217f75
Joonghwa <joonghwa@example.com>
2025-04-10 17:14:21 +0900

    첫 업로드: Flask 공유 폴더 앱

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, send_from_directory, redirect, url_for
+
+import os
+app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+@app.route('/')
+def home():
+    return '''
+    <h2>파일 업로드</h2>
+    <form method="POST" action="/upload" enctype="multipart/form-data">
+      <input type="file" name="file">
+      <input type="submit" value="업로드">
+    </form>
+    <br>
+    <a href="/files">파일 목록 보기</a>
+    '''
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    if file:
+        file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    return redirect(url_for('home'))
+
+@app.route('/files')
+def list_files():
+    files = os.listdir(UPLOAD_FOLDER)
+    links = [f'<li><a href="/download/{f}">{f}</a></li>' for f in files]
+    return f'<h2>파일 목록</h2><ul>{"".join(links)}</ul><a href="/">← 업로드</a>'
+
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
